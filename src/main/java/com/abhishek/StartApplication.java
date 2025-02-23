3b3de169e55e93b02277ad254c87e9052d17f869
rajk68501 <63809755+rajk68501@users.noreply.github.com>
2025-02-23 14:04:29 +0530

    Update StartApplication.java

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -12,13 +12,31 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
+        // Add other attributes
         model.addAttribute("title", "This is a SpringBoot Static Web Application");
-        model.addAttribute("msg", "Application Is Deployed To Kuberneets");
-        return "index";
+        model.addAttribute("msg", "Application Is Deployed To Kubernetes");
+
+        // Add form object to the model (this could be a model object with name field)
+        model.addAttribute("form", new MyForm());  // Passing form to the view
+        
+        return "index"; // Return the template
     }
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
 
+    // Define a form class with a name property (assuming form.name is used)
+    public static class MyForm {
+        private String name;
+
+        // Getter and setter
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
